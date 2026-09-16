3a370c9bb10ab6517527ae3e3204817cdcc3f2b8
Amith shetty M <amithshettym7@gmail.com>
2026-09-16 22:59:27 +0530

    Updated portfolio

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-red-800 h-10 flex gap-4 '>
+      My Portfolio About Projects Skills Contact Education Experience
+    </div>
+  )
+}
+
+export default Navbar
